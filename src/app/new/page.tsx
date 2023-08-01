bc2438c2ae65ab1c02b7e30da86fa7c9e12aaa07
alternateAyush <ayushbhatt2002@gmail.com>
2023-08-02 01:00:21 +0530

    Home Page

---
+++ src/app/new/page.tsx
@@ -0,0 +1,4 @@
+export default function NewPage()
+{
+    return <h1>New</h1>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,25 @@
-export default function Home()
+import { prisma } from '@/prisma'
+import Link from 'next/link'
+
+function getTodos()
 {
-  return (<h1>Hi</h1>)
+  return prisma.todo.findMany();
+}
+
+export default async function Home () {
+  const todos=await getTodos();
+  return (
+    <>
+      <header className='flex justify-between items-center mb-4'>
+        <h1 className='text-2xl'>Todos</h1>
+        <Link
+          href='/new'
+          className='border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none'
+        >
+          New
+        </Link>
+      </header>
+      <ul></ul>
+    </>
+  )
 }
